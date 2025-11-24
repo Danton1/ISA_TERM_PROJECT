29d79200d9c1e3415b4a39edf8421b2da97c3632
Danton Soares <dantonvsoares@gmail.com>
2025-11-23 19:48:04 -0800

    Adding a button to go to the admin page if the user is an admin, improving the main page UI and dashboard UI

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession, signOut } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
@@ -10,16 +10,52 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { MESSAGES } from "@/constants/lang/messages";
 
 export default function DashboardPage() {
   const router = useRouter();
   const { data: session, isPending } = useSession();
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     if (!isPending && !session) router.replace("/login");
   }, [isPending, session, router]);
 
-  if (isPending) return <div className="p-6">Loading…</div>;
+    useEffect(() => {
+    async function checkRole() {
+      try {
+        const res = await fetch("/api/admin/me");
+        const data = await res.json();
+
+        if (!data.user || data.user.role !== "admin") {
+          setIsAdmin(false);
+        } else {
+          setIsAdmin(true);
+        }
+      } catch (err) {
+        setIsAdmin(false);
+        console.log(err);
+      }
+    }
+
+    checkRole();
+  }, []);
+
+
+  if (isPending) {
+    return (
+      <div className="min-h-dvh flex items-center justify-center p-6">
+        <div className="flex items-center gap-3">
+          <div
+            className="h-6 w-6 rounded-full border-4 border-gray-200 border-t-slate-700 animate-spin"
+            role="status"
+            aria-label="loading"
+          />
+          <span>{MESSAGES.general.loading}</span>
+        </div>
+      </div>
+    );
+  }
   if (!session) return null;
 
   return (
@@ -39,6 +75,18 @@ export default function DashboardPage() {
             >
               Education Advisor
             </Button>
+            {
+              isAdmin ? (
+                <Button
+                  className="bg-green-700 text-white hover:bg-green-600 hover:cursor-pointer"
+                  onClick={() => {
+                    router.push("/admin");
+                  }}
+                >
+                  Admin Panel
+                </Button>
+              ) : null
+            }
             <Button
               className="bg-red-700 text-white hover:bg-red-600 hover:cursor-pointer"
               onClick={async () => {
